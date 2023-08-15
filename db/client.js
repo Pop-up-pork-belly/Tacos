ca69d57808fe3aecd3207f6d791ff295f0410c5e
Sage <blazemacroz@gmail.com>
2023-08-14 20:19:37 -0500

    removing added code

--- db/client.js
+++ db/client.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-// this is testinghttps://fakerjs.dev/api/internet.html#username
+
 const connectionString =
   process.env.DATABASE_URL ||
   "postgresql://postgres:messi3214@localhost:5432/esports";
