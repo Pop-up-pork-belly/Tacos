9b86cbd4a334c48ccb480ea08ef0756e2cf82d4e
Blazemacroz <126203108+Blazemacroz@users.noreply.github.com>
2023-08-17 18:01:18 -0500

    Delete localhostconnect.js
    need to delete localhostconnect for gitignore to work

--- localhostconnect.js
+++
@@ -1,4 +0,0 @@
-const localHostConnection = "postgresql://postgres:tolkien@localhost:5432/esports"
-   // syntax = postgresql://USERNAME:PASSWORD@HOST:PORT/DATABASE
-
-module.exports = {localHostConnection}
