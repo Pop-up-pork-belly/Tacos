b805a9f459226a7bca622357f12566a6a67a39b3
Sage <blazemacroz@gmail.com>
2023-08-30 20:31:57 -0500

    added stripe

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { loadStripe } from '@stripe/stripe-js';
+
 
 import {
   Navbar,
@@ -17,6 +19,7 @@ import {
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [token, setToken] = useState(localStorage.getItem("token") || "");
+  const stripe = loadStripe('pk_test_51NkzIOAVE3vEHYrbJdPRjQRMIxyakUy1R7YDGROCugM5T2Idi6GedvBcK8BZcW3Qu1wGNFz2YS2JUUs8wJsADbjk00GSSnTZbi')
 
   useEffect(() => {
     localStorage.setItem("token", token);
