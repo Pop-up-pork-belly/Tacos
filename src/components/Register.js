f0e62b6b6897fb02fa693507afdaf88d935aee8b
Sage <blazemacroz@gmail.com>
2023-09-05 21:08:36 -0500

    fixed email not defined error

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import { BASE_URL } from "./api";
+import { Container, Typography, TextField, Button, Grid } from '@mui/material';
+import { borderColor } from "@mui/system";
+require("dotenv").config();
+
 
 const Register = ({token, setToken }) => {
     const [newUsername, setNewUsername] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [newEmail, setNewEmail] = useState("");
+    const [email, setEmail] = useState("");
     const [isAdmin, setIsAdmin] = useState(false)
 
     const handleSubmit = (event) => {
@@ -14,7 +17,7 @@ const Register = ({token, setToken }) => {
         const newUsername = event.target.newUsername.value;
         const newPassword = event.target.newPassword.value;
         const confirmPassword = event.target.confirmPassword.value;
-        const newEmail = event.target.newEmail.value;
+        const email = event.target.email.value;
 
         if (newUsername === "") {
             return alert("Please enter a Username")
@@ -25,19 +28,19 @@ const Register = ({token, setToken }) => {
         if (confirmPassword !== newPassword) {
             return alert("Passwords do not match")
         }
-        if (newEmail === "") {
+        if (email === "") {
             return alert("Please enter a Email Address")
         }
 
         setNewUsername("");
         setNewPassword("");
         setConfirmPassword("");
-        setNewEmail("");
+        setEmail("");
         setIsAdmin(false);
 
         const registerUser = async () => {
             try {
-                const response = await fetch(`${BASE_URL}users/register`, {
+                const response = await fetch(`${DATABASE_URL}users/register`, {
                     method: "POST",
                     headers: {
                         "Content_Type": "application/json",
@@ -46,7 +49,7 @@ const Register = ({token, setToken }) => {
                         user: {
                             username: `${newUsername}`,
                             password: `${newPassword}`,
-                            email: `${newEmail}`,
+                            email: `${email}`,
                             isAdmin: false,
                         },
                     }),
@@ -69,48 +72,79 @@ const Register = ({token, setToken }) => {
         setNewPassword(event.target.value);
       };
       const handleConfirmChange = (event) => {
-        setConfirmedPassword(event.target.value);
+        setConfirmPassword(event.target.value);
       };
       const handleEmailChange = (event) => {
-        setNewEmail(event.target.value);
-      }
-return (
-    <div>
-      {
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            name="newUsername"
-            value={newUsername}
-            onChange={handleNameChange}
-          />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="text"
-            name="newPassword"
-            value={newPassword}
-            onChange={handlePassChange}
-          />
-          <label htmlFor="confirm password">Confirm Password:</label>
-          <input
-            type="text"
-            name="confirmPassword"
-            value={confirmPassword}
-            onChange={handleConfirmChange}
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            name="email"
-            value={newEmail}
-            onChange={handleEmailChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      }
-    </div>
-  );
-    };
+        setEmail(event.target.value);
+      };
+      return (
+        <Container maxWidth="sm">
+          <Grid container justifyContent="center" alignItems="center">
+            <Grid item xs={12}>
+              <Typography variant="h5" align="center" style={{ color: 'white' }}>
+                Register Users
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <form onSubmit={handleSubmit}>
+                <div style={{ marginBottom: '16px' }}>
+                  <TextField
+                    label="Username"
+                    variant="outlined"
+                    fullWidth
+                    name="newUsername"
+                    value={newUsername}
+                    onChange={handleNameChange}
+                    InputLabelProps={{ style: { color: 'white' } }}
+                    InputProps={{ style: { color: 'white' } }}
+                  />
+                </div>
+                <div style={{ marginBottom: '16px' }}>
+                  <TextField
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                    type="password"
+                    name="newPassword"
+                    value={newPassword}
+                    onChange={handlePassChange}
+                    InputLabelProps={{ style: { color: 'white' } }}
+                    InputProps={{ style: { color: 'white' } }}
+                  />
+                </div>
+                <div style={{ marginBottom: '16px' }}>
+                  <TextField
+                    label="Confirm Password"
+                    variant="outlined"
+                    fullWidth
+                    type="password"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleConfirmChange}
+                    InputLabelProps={{ style: { color: 'white' } }}
+                    InputProps={{ style: { color: 'white' } }}
+                  />
+                </div>
+                <div style={{ marginBottom: '16px' }}>
+                  <TextField
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    name="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    InputLabelProps={{ style: { color: 'white' } }}
+                    InputProps={{ style: { color: 'white' } }}
+                  />
+                </div>
+                <Button variant="contained" color="primary" type="submit" fullWidth>
+                  Submit
+                </Button>
+              </form>
+            </Grid>
+          </Grid>
+        </Container>
+      );
+    }
 
 export default Register;
